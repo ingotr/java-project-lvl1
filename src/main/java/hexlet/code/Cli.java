e755cf06cea8914dd44e9568d6fe270aead273ec
amarkovkin <markovkin88@gmail.com>
2022-06-22 22:18:37 +0300

    9. fix: make scanner private

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void greetUser() {
         System.out.print("May I have your name? ");

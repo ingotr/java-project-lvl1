264b9846fa4742849be40cac1c146fa6dad19ae2
Markovkin Aleksandr <markovkin88@gmail.com>
2022-06-23 15:52:47 +0300

    16. fix: refactor methods

--- src/main/java/hexlet/code/Even.java
+++ src/main/java/hexlet/code/Even.java
@@ -1,39 +1,38 @@
 package hexlet.code;
 
 import java.util.Random;
-import java.util.Scanner;
-
 public class Even {
     private static final int MAX_GAME_COUNT = 3;
     public static void play() {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        String userName = Cli.greetUser();
         System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
-        runGameplay(random, scanner, userName);
+        runGameplay();
     }
 
-    private static void runGameplay(Random random, Scanner scanner, String userName) {
+    private static void runGameplay() {
         int gameCount = 0;
+        String userName = Cli.greetUser();
         do {
-            int number = random.nextInt(100) + 1;
-            System.out.format("Question: %d%n", number);
-
-            String userInput = scanner.nextLine().trim();
-            System.out.format("Your answer: %s%n", userInput);
-
-            if (runGameCycle(userName, number, userInput)) { break; }
+            if (runGameCycle(userName)) { break; }
             gameCount++;
         } while (gameCount < MAX_GAME_COUNT);
         System.out.format("Congratulations, %s!%n", userName);
     }
 
-    private static boolean runGameCycle(String userName, int number, String userInput) {
-        if (userInput.equals(isNumberEven(number))) {
+    private static String getCorrectAnswer() {
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        System.out.format("Question: %d%n", number);
+        return isNumberEven(number);
+    }
+
+    private static boolean runGameCycle(String userName) {
+        String correctAnswer = getCorrectAnswer();
+        String userInput = Cli.getUserInput();
+        if (userInput.equals(correctAnswer)) {
             System.out.println("Correct!");
         } else {
-            System.out.format("%s is wrong answer ;(. Correct answer was 'no'. Let's try again, %s!%n",
-                    userInput, userName);
+            System.out.format("%s is wrong answer ;(. Correct answer was %s. Let's try again, %s!%n",
+                    userInput, correctAnswer, userName);
             return true;
         }
         return false;

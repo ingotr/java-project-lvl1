9cd0769ba5a22522d4f0c17b36acdd72019f97e7
Markovkin Aleksandr <markovkin88@gmail.com>
2022-06-25 14:05:15 +0300

    27. fix: introduce constants

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -7,17 +7,21 @@ import static hexlet.code.Engine.getMaxNum;
 
 public class Progression extends Game {
 
+    public static final int MIN_PROGRESSION_LENGTH = 5;
+    public static final int PROGRESSION_LENGTH_INCREMENT = 5;
+    public static final int MAX_PROGRESSION_DIFFERENCE = 10;
+
     public Progression() {
         this.setWelcomeText("What number is missing in the progression?");
     }
 
-    public String getCorrectAnswer() {
+    public final String getCorrectAnswer() {
         Random random = new Random();
-        int progressionLength = random.nextInt(5) + 5;
+        int progressionLength = random.nextInt(MIN_PROGRESSION_LENGTH) + PROGRESSION_LENGTH_INCREMENT;
         int firstNumber = random.nextInt(getMaxNum()) + 1;
         String[] progression = new String[progressionLength];
 
-        int commonDifference = random.nextInt(10) + 1;
+        int commonDifference = random.nextInt(MAX_PROGRESSION_DIFFERENCE) + 1;
         int currentItem = firstNumber;
         progression[0] = String.valueOf(currentItem);
         for (int i = 1; i < progressionLength; i++) {
